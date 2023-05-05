cea61039b36f645495ba89c59a6ef5604cd20d4b
GJ Tiquia <gershom.tiquia@gmail.com>
2023-05-05 21:19:56 +0800

    chore: added babel-plugin-module-resolver

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,15 +1,10 @@
-import { Text } from "react-native"
-import { SafeAreaView as RNSafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
-import { styled } from 'nativewind';
-
-const SafeAreaView = styled(RNSafeAreaView);
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { HelloWorld } from "src/components"
 
 const Main = () => {
     return (
         <SafeAreaProvider>
-            <SafeAreaView className="flex-1 items-center justify-center">
-                <Text className="font-bold text-3xl">Hello World</Text>
-            </SafeAreaView>
+            <HelloWorld />
         </SafeAreaProvider>
     );
 }
